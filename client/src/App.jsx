569895850e7ac91bf750dd8e414fad16dc3e0d9e
Niwesh Baraj <niweshbaraj@gmail.com>
2023-11-10 19:10:53 +0530

    install react js, tailwind css and create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-orange-600 bg-black text-center font-bold underline">Hello !</h1>
+    </>
+  )
+}
+
+export default App
